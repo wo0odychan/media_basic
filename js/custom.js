a0ce7c663118494b20ea1de45bd61789010f6ff3
wo0odychan <wo0ody@kakao.com>
2023-06-30 14:07:59 +0900

    20230630

--- js/custom.js
+++ js/custom.js
@@ -15,11 +15,37 @@ rightArrow.addEventListener('click', function () {
 
 const proSlide = new Swiper('.pro_slide', {
     loop: true,
-    slidesPerView: 4,
+    slidesPerView: 1,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
     spaceBetween: 30,
     pagination: {
         el: ".sbar",
         type: "progressbar",
     },
 
+    breakpoints: {
+        540: {
+            slidesPerView: 4,
+        }
+    }
+
+});
+
+
+const mobaileBtn = document.querySelector('.mobile_btn');
+const GNB = document.querySelector('#gnb');
+const H1 = document.querySelector('h1');
+
+mobaileBtn.addEventListener('click', function () {
+    GNB.classList.toggle('on');
+    H1.classList.toggle('on');
+});
+
+GNB.addEventListener('wheel', function (e) {
+    if (GNB.classList.contains('on')) {
+        e.preventDefault();
+    }
 })
